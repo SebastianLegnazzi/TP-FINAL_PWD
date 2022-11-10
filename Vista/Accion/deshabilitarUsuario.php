fd0dc427d5443d154b7629137ed1d21b00504348
FlorÑonquepan <florencianionquepan@outlook.com>
2022-11-10 19:17:40 -0300

    agrega acción deshabilitar usuario

--- Vista/Accion/deshabilitarUsuario.php
+++ Vista/Accion/deshabilitarUsuario.php
@@ -1,5 +1,12 @@
 <?php
 include_once("../../configuracion.php");
 
-
+$datos=data_submitted();
+$objUsuario=new C_Usuario();
+$usuarioModificado=$objUsuario->deshabilitar($datos);
+if($usuarioModificado){
+    echo json_encode(array('success'=>1));
+}else{
+    echo json_encode(array('success'=>0));
+}
 ?>
