2eac394e2ffe985f2256529dba97e58e685c5b5d
FlorÑonquepan <florencianionquepan@outlook.com>
2022-11-10 19:58:17 -0300

    Actualizo ajax de deshabilitarUsuario

--- Vista/js/deshabilitarUsuarios.js
+++ Vista/js/deshabilitarUsuarios.js
@@ -1,21 +1,62 @@
-$(document).ready(function() {
-    $('#deshabilitar').onclick(function() {
-        $.ajax({
-            type: "POST",
-            url: '../Accion/deshabilitarUsuario.php',
-            data: $(this).serialize(),
-            success: function(response){
-                var jsonData = JSON.parse(response);
- 
-                // user is logged in successfully in the back-end
-                // let's redirect
-                if (jsonData.success == "1"){
-                    deshabilitarSuccess();
-                }
-                else if (jsonData.success == "0"){
-                    deshabilitarFailure();
-                }
-           }
-       });
-     });
-});
+$(document).on('click', '.deshabilitar', function() {
+    const form=$('form');
+    mostrarAdvertencia(form);
+}
+)
+
+function mostrarAdvertencia(form){
+    Swal.fire({
+        title: 'Realmente quiere deshabilitar el usuario?',
+        showDenyButton: true,
+        confirmButtonText: 'Si',
+        denyButtonText: `Cancelar`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          deshabilitarUsuario(form);
+        } else if (result.isDenied) {
+          Swal.fire('El usuario no fue deshabilitado', '', 'info')
+        }
+      })
+}
+
+function deshabilitarUsuario(form){
+    $.ajax({
+        type: "POST",
+        url: '../Accion/deshabilitarUsuario.php',
+        data: form.serialize(),
+        success: function(response){
+            var jsonData = JSON.parse(response);
+
+            // user is logged in successfully in the back-end
+            // let's redirect
+            if (jsonData.success == "1"){
+                deshabilitarSuccess();
+            }
+            else if (jsonData.success == "0"){
+                deshabilitarFailure();
+            }
+       }
+   });
+}
+
+function deshabilitarSuccess(){
+    Swal.fire({
+        icon: 'success',
+        title: 'Se deshabilito el usuario correctamente!',
+        showConfirmButton: false,
+        timer: 1500
+    })
+    setTimeout(function(){
+        location.reload();
+    },1500);
+}
+
+function deshabilitarFailure(){
+    Swal.fire({
+        icon: 'error',
+        title: 'No se pudo deshabilitar el usuario',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
