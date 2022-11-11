494028941e5fe2a9e9d76cda8e6187430ee01638
FlorÑonquepan <florencianionquepan@outlook.com>
2022-11-11 11:59:04 -0300

    Modifica estilos de listarUsuarios

--- Vista/sesion/listaUsuarios.php
+++ Vista/sesion/listaUsuarios.php
@@ -18,14 +18,15 @@ $i = 0;
     if ($cantUsers > -1) {
     ?>
         <div class="border rounded p-3 mb-2 bg-dark text-white">
-        <table class="table table-dark table-hover P-5">
-            <thead>
+        <table class="table table-dark table-hover p-5">
+            <thead class="text-center">
             <tr>
-            <th scope="col-6">USER</th>
-            <th scope="col-6">USERNAME</th>
+            <th scope="col-6">USUARIO</th>
+            <th scope="col-6">NOMBRE</th>
             <th scope="col-4">MAIL</th>
             <th scope="col-4">ROL</th>
-            <th scope="col-2">ACTIONS</th>
+            <th scope="col-4">DESHABILITADO</th>
+            <th scope="col-2">ACCIONES</th>
             </tr>
             </thead>
             <tbody>
@@ -33,7 +34,7 @@ $i = 0;
             while ($i < $cantUsers) {
             ?>
                 <tr> 
-                    <th scope="row"><?php echo $i+1 ?></th>
+                    <th scope="row" class="text-center"><?php echo $i+1 ?></th>
                     <td><?php echo $arrayUsers[$i]->getUsNombre() ?></td>
                     <td> <?php echo $arrayUsers[$i]->getUsMail() ?> </td>
                     <td> <?php $msn="-"; 
@@ -42,13 +43,20 @@ $i = 0;
                         } 
                         echo $msn ?> 
                     </td>
+                    <td class="text-center"> <?php echo $arrayUsers[$i]->getUsDeshabilitado()==NULL?"NO":"SI";?> </td>
                     <td>
-                        <form method='post' action='actualizarLogin.php'>
+                        <form method='post'>
                         <input style="display:none;" name='idUsuario' id='idUsuario' value='<?php echo $arrayUsers[$i]->getIdUsuario()?>'>
-                        <button class="text-decoration-none btn btn-outline-warning"> EDITAR </button>
-                        <button type="submit" class="text-decoration-none btn btn-outline-danger deshabilitar">
+                        <button type="button" class="ms-3 text-decoration-none btn btn-outline-warning"> EDITAR </button>
+                        <?php echo $arrayUsers[$i]->getUsDeshabilitado()==NULL?
+                        "<button type='button' class='mx-2 text-decoration-none btn btn-outline-danger deshabilitar'>
                         DESHABILITAR
-                        </button>
+                        </button>":
+                        "<button style='display:none;'type='button' class='mx-2 text-decoration-none btn btn-outline-danger habilitar'>
+                        HABILITAR
+                        </button>";
+                        ?>
+                        
                         </form>
                     </td>
                 </tr>
