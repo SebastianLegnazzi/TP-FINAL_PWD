134ed7ed4a0e2938decd54cb7090d7b672d9aafe
FlorÑonquepan <florencianionquepan@outlook.com>
2022-11-11 14:30:35 -0300

    Se implementa ajax en actualizarUsuario

--- Vista/sesion/actualizarLogin.php
+++ Vista/sesion/actualizarLogin.php
@@ -8,8 +8,8 @@ $usuarioModificar=$objUsuario->buscar($datos);
 $descRolesUsuario=$objUsuarioRol->darDescripcionRoles($usuarioModificar);
 ?>
 <div class="container-md">
-<main class="form-signin w-50 m-auto mt-5 text-center">
-    <form class="row gy-2 text-center justify-content-center rounded bg-dark text-white" method="post" action="../Accion/loginActualizado.php">
+<main class="w-50 m-auto mt-5 text-center">
+    <form class="row gy-2 text-center justify-content-center rounded bg-dark text-white needs-validation" novalidate>
     <div class="col-10" style="display:none;">
         <label for="floatingInput" class="form-label mt-2">ID</label>
         <input type="number" class="form-control" 
@@ -18,22 +18,22 @@ $descRolesUsuario=$objUsuarioRol->darDescripcionRoles($usuarioModificar);
     <div class="col-10 col-lg-7">
         <label for="floatingInput" class="form-label mt-2">NOMBRE DE USUARIO</label>
         <input type="text" class="form-control" placeholder="Username" 
-                name="usNombre" id="usNombre" value="<?php echo $usuarioModificar[0]->getUsNombre()?>">
+                name="usNombre" id="usNombre" value="<?php echo $usuarioModificar[0]->getUsNombre()?>" required>
     </div>
     <div class="col-10 col-lg-7">
     <label for="usNombre" class="form-label mt-2">MAIL</label>
         <input type="text" class="form-control" placeholder="Mail" 
-                name="usMail" id="usMail" value="<?php echo $usuarioModificar[0]->getUsMail()?>">
+                name="usMail" id="usMail" value="<?php echo $usuarioModificar[0]->getUsMail()?>" required>
     </div>
     <div class="col-10 col-lg-7">
     <label for="usPass" class="form-label mt-2">CONTRASEÑA</label>
         <input type="password" class="form-control" placeholder="***********" 
-                name="usPass" id="usPass">
+                name="usPass" id="usPass" value="<?php echo $usuarioModificar[0]->getUsPass()?>"required>
     </div>
     <div class="col-8 col-lg-7 mt-4">
         <h6 class="text-center mb-3">ROLES</h6>
         <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="ROLE_USER" id="user" name="rol[]" checked>
+            <input class="form-check-input" type="checkbox" value="ROLE_USER" id="user" name="rol[]" checked readonly>
             <label class="form-check-label" for="user">
                 USER
             </label>
@@ -54,12 +54,12 @@ $descRolesUsuario=$objUsuarioRol->darDescripcionRoles($usuarioModificar);
             </label>
         </div>
     </div>
-
     <button class="btn btn-lg btn-success my-3 col-10 col-lg-7 mt-4">MODIFICAR</button>
-
     </form>
 </main>
 </div>
+<script src="../js/validarCamposVacios.js"></script>
+<script src="../js/mainActualizarLogin.js"></script>
 <?php
 include_once("../estructura/Pie.php")
 ?>
