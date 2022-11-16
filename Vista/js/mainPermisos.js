fa11db621d5712483e758b4a904884a60c83f614
FlorÑonquepan <florencianionquepan@outlook.com>
2022-11-16 17:22:08 -0300

    agrego función modificar()

--- Vista/js/mainPermisos.js
+++ Vista/js/mainPermisos.js
@@ -1,11 +1,3 @@
-function eliminar(id){
-    
-}
-
-function modificar(id){
-    
-}
-
  $(document).ready(function () {
     $('#formNuevoPermiso').submit(function (e) {
         e.preventDefault();
@@ -42,7 +34,7 @@ function permisoSuccess() {
         timer: 1500
     })
     setTimeout(function () {
-        location.reload();
+        location.href='gestionarPermisos.php';
     }, 1500);
 }
 
@@ -51,6 +43,64 @@ function permisoFailure() {
     Swal.fire({
         icon: 'error',
         title: 'El permiso no pudo ser creado',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
+
+function deshabilitar(){
+    
+}
+
+function modificar(){
+    $(document).ready(function () {
+        $('#formModificarPermiso').submit(function (e) {
+            e.preventDefault();
+            const forms = document.querySelectorAll('.needs-validation');
+            if (forms[0].checkValidity()) {
+                $.ajax({
+                    type: "POST",
+                    url: '../Accion/modificarPermiso.php',
+                    data: $(this).serialize(),
+                    success: function (response) {
+                        var jsonData = JSON.parse(response);
+                        // user is logged in successfully in the back-end
+                        // let's redirect
+                        if (jsonData.success == "3"){
+                            modificadoSuccess("Rol y datos");
+                        }else if (jsonData.success == "2"){
+                            modificadoSuccess("Datos del menu");
+                        }else if(jsonData.success == "1"){
+                            //solo modifico roles: Rol agregado con exito!
+                            modificadoSuccess("Rol")
+                        }else if(jsonData.success == "0"){
+                            modificadoFailure();
+                        }
+                    }
+                });
+            } else {
+                forms[0].classList.add('was-validated');
+            }
+        });
+    });
+}
+
+function modificadoSuccess(string){
+    Swal.fire({
+        icon: 'success',
+        title: `${string} modificados con exito!`,
+        showConfirmButton: false,
+        timer: 1500
+    })
+    setTimeout(function () {
+        location.reload();
+    }, 1500);
+}
+
+function modificadoFailure() {
+    Swal.fire({
+        icon: 'error',
+        title: 'No se modificó ningún dato',
         showConfirmButton: false,
         timer: 1500
     })
