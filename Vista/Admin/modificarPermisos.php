d07779931e407a4dac3df180609a4681ede4ac80
FlorÑonquepan <florencianionquepan@outlook.com>
2022-11-16 17:52:26 -0300

    Se actualiza la vista de modificarPermiso

--- Vista/Admin/modificarPermisos.php
+++ Vista/Admin/modificarPermisos.php
@@ -8,37 +8,49 @@ $objRol=new C_Rol();
 $menuModificar=$objMenu->buscar($datos);
 $rolModificar=$objMenuRol->buscar($datos);
 $roles=$objRol->buscar(null);
+$permisos=$objMenuRol->buscar(null);
 //$descRolesUsuario=$objMenuRol->darDescripcionRoles($usuarioModificar);
 ?>
 
 
 <div class="container-md">
 <main class="w-50 m-auto mt-5 text-center">
-    <form class="row gy-2 text-center justify-content-center rounded bg-dark text-white needs-validation" novalidate>
+    <form class="row gy-2 text-center justify-content-center rounded bg-dark text-white needs-validation my-5" 
+        id="formModificarPermiso" method="post" action="../Accion/modificarPermiso.php" novalidate>
     <div class="col-10" style="display:none;">
         <label for="floatingInput" class="form-label mt-2">ID</label>
         <input type="number" class="form-control" 
                 name="idMenu" value="<?php echo $menuModificar[0]->getIdMenu()?>">
     </div>
     <div class="col-10 col-lg-7">
-        <label for="floatingInput" class="form-label mt-2">NOMBRE DE MENU</label>
-        <input type="text" class="form-control" placeholder="Username" 
+        <label for="meNombre" class="form-label mt-2">Nombre de Menu</label>
+        <input type="text" class="form-control" pattern="[a-zA-Z]+\s?[a-zA-Z]*\s?[a-zA-Z]*\s?[a-zA-Z]*\s?[a-zA-Z]*"
                 name="meNombre" value="<?php echo $menuModificar[0]->getMeNombre()?>" required>
     </div>
     <div class="col-10 col-lg-7">
-    <label for="usNombre" class="form-label mt-2">ID PADRE</label>
-        <input type="text" class="form-control" placeholder="Mail" 
-                name="idPadre" value="<?php echo $menuModificar[0]->getPadre()?>" required>
-    </div>
+                
+                <label class="form-label mt-2">Menu Padre</label>
+                <select name="idPadre" class="form-select" aria-label=".form-select example">
+                <option value="" selected >Seleccionar</option>
+                <?php
+                foreach($permisos as $permiso){
+                    ?>
+                    <option value="<?php echo $permiso->getMenu()->getIdMenu()?>"> <?php echo $permiso->getMenu()->getMeNombre()?></option>
+                    <?php
+                }
+                ?>
+                </select>
+                
+            </div>
     <div class="col-10 col-lg-7">
-    <label for="usPass" class="form-label mt-2">RUTA RELATIVA</label>
-        <input type="text" class="form-control"
-                name="meDescripcion" value="<?php echo $menuModificar[0]->getMeDescripcion()?>"required>
+    <label for="meDescripcion" class="form-label mt-2">RUTA RELATIVA</label>
+        <input type="text" class="form-control" pattern="(..\/)([a-zA-Z0-9_\-]+)(\/[a-zA-Z0-9_\-]+)\.php"
+                name="meDescripcion" value="<?php echo $menuModificar[0]->getMeDescripcion()?>" required>
     </div>
     <div class="col-8 col-lg-7 mt-4">
         <label>Rol que puede acceder</label>
-                <select class="form-select mb-3" aria-label=".form-select example">
-                <option name="rol[]" selected><?php echo $rolModificar[0]->getRol()->getRolDescripcion() ?></option>
+                <select name="idRol" class="form-select mb-3" aria-label=".form-select example" required>
+                <option value="<?php echo $rolModificar[0]->getRol()->getIdRol() ?>" selected><?php echo $rolModificar[0]->getRol()->getRolDescripcion() ?></option>
                 <?php
                 foreach($roles as $rol){
                     if($rol->getRolDescripcion()!=$rolModificar[0]->getRol()->getRolDescripcion()){
@@ -50,12 +62,12 @@ $roles=$objRol->buscar(null);
                 ?>
                 </select>
     </div>
-    <button onclick="modificar()" class="btn btn-lg btn-success my-3 col-10 col-lg-7 mt-4">MODIFICAR</button>
+    <button class="btn btn-lg btn-success my-3 col-10 col-lg-7 mt-4">MODIFICAR</button>
     </form>
 </main>
 </div>
 
-<script src="../js/mainPermisos.js"></script>
+<script src="../js/modificarPermisos.js"></script>
 <?php
 include_once("../estructura/Pie.php")
 ?>
