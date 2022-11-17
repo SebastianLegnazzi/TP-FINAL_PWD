fe0e1ff3f9665ab37db8999a0fb1f3925bb02a23
Dariana <darianagsm@gmail.com>
2022-11-16 21:08:29 -0300

    cambiar contraseña

--- Vista/Cliente/perfil.php
+++ Vista/Cliente/perfil.php
@@ -1,7 +1,8 @@
 <?php
 include_once('../estructura/Cabecera.php');
 
-$datos['UsNombre'] = $_SESSION['nombreUsuario'];
+$datos['usNombre'] = $_SESSION['nombreUsuario'];
+
 $usuario = new C_Usuario;
 $usuario = $usuario->buscar($datos)[0];
 
@@ -68,9 +69,87 @@ $usuario = $usuario->buscar($datos)[0];
             </form>
         </div>
     </div>
+
+    <div class="container-fluid col-md-9 text-white mt-5 d-none" id='editarContraseña'>
+        <h2>Cambiar Contraseña:</h2>
+        <div class="mb-3">
+            
+        <form  id='form-contraseña' method="post" action="../Accion/accionActualizarPerfil.php"class="needs-validation row text-white justify-content-center col-12" novalidate>
+                <table class="table table-striped table-dark">
+                    <tr>
+                         
+                        <th>Username:</th>
+                        <th>Ingrese contraseña actual:</th>
+                        <th>Ingrese contraseña nueva:</th>
+                        <th>Confirmar contraseña:</th>
+                    
+                    </tr>
+ 
+                    <tr>
+
+                    <td><div class="col-lg-7 col-12"><?php echo $usuario->getUsNombre()?></div>
+                        <div class="col-lg-7 col-12 d-none "><input  value = '<?php echo $usuario->getUsNombre()?>' type="text" name="usNombre"></input></div>
+                        <div class="col-lg-7 col-12 d-none"><input value = '<?php echo $usuario->getUsPass()?>' type="text" name="usPass"></input></div>
+                    </td>
+
+                    <td>
+                        <div class="col-lg-8 col-12"><input type="password" class="form-control" id="usPassVieja" name="usPassVieja" required>
+                        <div class="invalid-feedback">
+                                Ingrese una contraseña!
+                            </div>
+                            <div class="valid-feedback password-correcta">
+                            Correcto!
+                            </div>
+                    </div>
+
+                        <div class="col-lg-7 col-12 d-none"><input value = '<?php echo $usuario->getUsMail()?>' type="email" name="usMail" ></input></div>
+                        <div class="col-lg-7 col-12 d-none"><input value = '<?php echo $usuario->getIdUsuario()?>' type="number"  name="idUsuario"></input></div>
+                    </td>
+
+                    <td>
+                        <div class="col-lg-8 col-12"><input type="password" class="form-control" name="usPassNueva" id="usPassNueva" required>
+                            <div class="invalid-feedback">
+                                Ingrese una contraseña!
+                            </div>
+                            <div class="invalid-password" style="display: none; color: red;">
+                            Las contraseñas no coinciden
+                            </div>
+                            <div class="valid-feedback password-correcta">
+                            Correcto!
+                            </div>
+                        </div>
+                        <div class="col-10 col-lg-7 d-none"><input type="password" class="form-control" name="usPass" id="usPass">
+                        <div class="col-10 col-lg-7 d-none"><input type="text" id="usPassSesion" disabled value='<?php echo $usuario->getUsPass()?>' class="form-control" name="usPassSesion" >
+                        </div>
+                    </td>
+                    
+                    <td>
+                        <div class="col-lg-8 col-12"><input type="password" id="usPassRep" class="form-control" name="usPassRep" requierd> 
+                            <div class="invalid-feedback">
+                                Ingrese una contraseña!
+                            </div>
+                            <div class="invalid-password" style="display: none; color: red;">
+                                Las contraseñas no coinciden
+                            </div>
+                            </div>
+                    </td>
+
+                    
+                    </tr>
+            
+
+                </table>
+
+                <input class="btn btn-success mt-2 col-3" type="submit" name="boton_enviar"  id="boton_enviar" value="GUARDAR">
+                <input class="btn btn-danger mx-4 mt-2 col-3" name="boton_cancelar" type="button" id="boton_cancelar" value="CANCELAR">
+            </form>
+        </div>
+    </div>
 </div>
 
 <script src="../js/perfil.js"></script>
+<script src="../js/validarContraseñaIguales.js"></script>
+<script src="../js/md5.js"></script>
 <?php
 
 include_once("../estructura/Pie.php")
--- Vista/js/perfil.js
+++ Vista/js/perfil.js
@@ -1,12 +1,14 @@
 $(document).on('click', '#boton_editarDatos', function() {
 
     document.getElementById('editarDatos').classList.remove('d-none');
+    document.getElementById('editarContraseña').classList.add('d-none');
 
 });
 
 $(document).on('click', '#boton_contra', function() {
 
     document.getElementById('editarDatos').classList.add('d-none');
+    document.getElementById('editarContraseña').classList.remove('d-none');
 
 });
 
@@ -14,11 +16,12 @@ $(document).on('click', '#boton_contra', function() {
 $(document).on('click', '#boton_cancelar', function() {
 
     document.getElementById('editarDatos').classList.add('d-none');
+    document.getElementById('editarContraseña').classList.add('d-none');
 
 });
 
 $(document).ready(function () {
-    $('form').submit(function (e) {
+    $('#form-editar').submit(function (e) {
         e.preventDefault();
         const forms = document.querySelectorAll('.needs-validation');
         if (forms[0].checkValidity()) {
@@ -45,6 +48,52 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $('#form-contraseña').submit(function (e) {
+        e.preventDefault();
+         const forms = document.querySelectorAll('.needs-validation');
+        var passActual = document.getElementById('usPassVieja').value;
+            passActual = hex_md5(passActual).toString();
+
+        var passSesion = document.getElementById('usPassSesion').value;
+        if(passActual == passSesion){
+                
+        if (verificarContraseñaIgual(document.getElementById('usPassNueva'), document.getElementById('usPassRep')) && forms[0].checkValidity()) {
+            var password = document.getElementById("usPassNueva").value;
+            var passhash = hex_md5(password).toString();
+
+            document.getElementById('usPassNueva').value = "";
+            document.getElementById('usPassRep').value = "";
+            document.getElementById('usPassVieja').value = "";
+            document.getElementById("usPass").value = passhash;
+            
+            $.ajax({
+                type: "POST",
+                url: '../Accion/accionActualizarPerfil.php',
+                data: $(this).serialize(),
+                success: function (response) {
+                    var jsonData = JSON.parse(response);
+
+                    // user is logged in successfully in the back-end
+                    // let's redirect
+                    if (jsonData.success == "1") {
+                        contraSucces();
+                        forms[1].reset();
+                    }
+                    else if (jsonData.success == "0") {
+                        contraFailure();
+                    } 
+                }
+            });
+        } else {
+            forms[1].classList.add('was-validated');
+        }
+    }else{
+        failureContra();
+        forms[1].reset();
+    }
+    });
+});
 
 function success() {
     Swal.fire({
@@ -62,6 +111,41 @@ function failure() {
     Swal.fire({
         icon: 'error',
         title: 'No se ha podido modificar el email! 😥',
+        showConfirmButton: false,
+        timer: 1500
+    })
+    setTimeout(function () {
+        location.reload();
+    }, 1500);
+}
+
+function failureContra() {
+    Swal.fire({
+        icon: 'error',
+        title: 'La contraseña no coincide con la actual! 😬',
+        showConfirmButton: false,
+        timer: 1500
+    })
+    setTimeout(function () {
+    }, 1500);
+}
+
+function contraFailure(){
+    Swal.fire({
+        icon: 'error',
+        title: 'No se ha podido modificar la contraseña! 😥',
+        showConfirmButton: false,
+        timer: 1500
+    })
+    setTimeout(function () {
+        location.reload();
+    }, 1500);
+}
+
+function contraSucces(){
+    Swal.fire({
+        icon: 'success',
+        title: 'Se ha modificado la contraseña! 😉',
         showConfirmButton: false,
         timer: 1500
     })
